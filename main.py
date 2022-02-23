4eee559be1ac2dd6ca64483e75527c598573fa82
Adrienne H Slaughter <ahandrew@gmail.com>
2022-02-23 00:25:27 +0000

    Back to basics.

--- main.py
+++ main.py
@@ -19,11 +19,9 @@ def main(winstyle = 0):
 
     ## Step 1: Load panda image
     panda_img = pygame.image.load('imgs/panda1.png')
-    sad_panda_img = pygame.image.load('imgs/sad_panda.png')
 
     ## Step LAST: Resize our panda!
     panda_img = pygame.transform.scale(panda_img, (100, 100))
-    sad_panda_img = pygame.transform.scale(sad_panda_img, (100, 100))
 
     ## Step 2: Get bounding rectangle
     panda_rect = panda_img.get_rect()
@@ -43,13 +41,11 @@ def main(winstyle = 0):
     y = 0
 
     step_size = 10
-    panda_is_hurt = False
 
     # Event loop
     while 1:
       x = 0
       y = 0
-      panda_is_hurt = False
       screen.blit(background, (0, 0))
 
       ## Paint obstacle on the screen
@@ -80,10 +76,7 @@ def main(winstyle = 0):
       panda_rect = panda_rect.move(x, y)
 
       if panda_rect.colliderect(spike_rect):
-        # print("ARGGGGGHHHHH")
-        # scream_sound.play()
-        panda_is_hurt = True
-        print ('x\a')
+        print("ARGGGGGHHHHH")
 
       # ## Step 5: Check that we're not off the screen
       # if (panda_rect.right > width or panda_rect.left < 0): 
@@ -92,12 +85,7 @@ def main(winstyle = 0):
       #   y = -y
 
       ## Step 3: Paint panda on gameboard
-      if panda_is_hurt: 
-        screen.blit(sad_panda_img, panda_rect)
-      else: 
-        screen.blit(panda_img, panda_rect)
-
-      
+      screen.blit(panda_img, panda_rect)
       
       pygame.display.flip()
 
